49288c2207e119fea92afce8083b281642830ac8
Yuriy Yakym <yakym.yuriy@gmail.com>
2017-09-03 16:24:34 +0200

    Handling promises and generators in state

--- src/view-model.js
+++ src/view-model.js
@@ -1,13 +1,14 @@
 import React, { PureComponent } from 'react';
+import { isFunction, isPromise, isGeneratorFn, isIterable } from './utils';
 
 export default function ViewModel(config) {
-  const getConfig = typeof config === 'function' ? config : () => config;
+  const getConfig = isFunction(config) ? config : () => config;
 
   return (View) => {
     class RealizeeComponent extends PureComponent {
       constructor(props, ...restArguments) {
         super(props, ...restArguments);
-        this.state = props.config.state;
+        this.state = manageState(props.config.state, this.setState.bind(this));
       }
 
       render() {
@@ -22,3 +23,36 @@ export default function ViewModel(config) {
     );
   }
 }
+
+const manageState = (state, setState) => Object.keys(state).reduce((result, property) => {
+  result[property] = manageStateProperty(state, property, setState);
+  return result;
+}, {});
+
+const manageStateProperty = (state, property, setState) => {
+  const value = state[property];
+
+  if(isPromise(value)) {
+    return handlePromise(state, property, setState);
+  }
+
+  if(isIterable(value)) {
+    return handleIterable(state, property);
+  }
+
+  if(isGeneratorFn(value)) {
+    return handleGenerator(state, property);
+  }
+
+  return value;
+};
+
+const handlePromise = (state, property, setState) => {
+  const promise = state[property];
+  promise.then(value => setState({ [property]: value }));
+  return undefined;
+};
+
+const handleGenerator = (state, property) => Array.from(state[property]());
+
+const handleIterable = (state, property) => Array.from(state[property]);
