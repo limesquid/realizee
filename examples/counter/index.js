586a0bc68b6d35776ff481b03bbe2b09c2b5e7cb
Yuriy Yakym <ziqueq@gmail.com>
2017-09-03 03:21:21 +0200

    Example update

---
+++ examples/counter/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './src';
+
+const rootElement = document.getElementById('app');
+ReactDOM.render(<Counter />, rootElement);
